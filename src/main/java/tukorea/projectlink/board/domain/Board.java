bc0d7fa6066f13eb33bde69e2a80cb1150a6ad6c
SangWoon123 <alrax3574@naver.com>
2024-03-25 18:36:49 +0900

    fix: Board 엔티티 comment 연관관계수정

--- src/main/java/tukorea/projectlink/board/domain/Board.java
+++ src/main/java/tukorea/projectlink/board/domain/Board.java
@@ -5,6 +5,11 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import tukorea.projectlink.comment.domain.Comment;
+import tukorea.projectlink.user.User;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Data
@@ -20,6 +25,13 @@ public class Board {
     private String title;
     @Column
     private String content;
+
+    @OneToMany(mappedBy = "board", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    List<Comment> comments=new ArrayList<>();
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
 
     public void update(String title, String content){
         this.title = title;
