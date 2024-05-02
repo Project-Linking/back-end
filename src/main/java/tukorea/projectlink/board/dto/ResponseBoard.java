de0dd96c67904b3b2b80f2c5547fd745df47750a
jjinbok <haba8730@naver.com>
2024-05-02 23:01:04 +0900

    feature:유효성 검사 추가

--- src/main/java/tukorea/projectlink/board/dto/ResponseBoard.java
+++ src/main/java/tukorea/projectlink/board/dto/ResponseBoard.java
@@ -1,5 +1,7 @@
 package tukorea.projectlink.board.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -9,7 +11,12 @@ import java.time.LocalDateTime;
 @Builder
 public class ResponseBoard {
     private Long id;
+
+    @NotBlank
+    @Size(min = 1, max = 30)
     private String title;
+
+    @Size(max = 3000)
     private String content;
     private LocalDateTime deadline;
 
